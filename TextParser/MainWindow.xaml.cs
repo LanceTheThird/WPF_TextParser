e01332a8eaf58d6c7a59da67a28b14837cd45abc
Vanya <Vanya@DESKTOP-504B951>
2018-12-18 23:21:52 +0200

    Add project files.

---
+++ TextParser/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using TextParser.Services;
+
+namespace TextParser
+{
+    /// <summary>
+    /// Я мог бы реализовать паттерн MVVM, например, но задача предельно простая, не вижу смысла так усложнять себе жизнь, да и вам
+    // Потому будем реализовывать логику тут, хотя я избегаю обычно трогать класс MainWindow, передавая её на ViewModel
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            TextParserService service = new TextParserService();
+            ResultWindow result = new ResultWindow(service.Parse(textBox.Text));
+            result.Show();
+            this.Close();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ TextParser/Services/TextParserService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TextParser.Services
+{
+    class TextParserService
+    {
+        public string Parse(string text)
+        {
+            string result = String.Empty;
+            string delimiter = String.Empty;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if ((!Regex.IsMatch(text[i].ToString(), "^\\d{1}$")) &&
+                        (!Regex.IsMatch(text[i].ToString(), @"[а-я]$")) &&
+                        (!Regex.IsMatch(text[i].ToString(), @"[a-z]$")))
+                {
+                    // Если это не цифра и не буква латинского алфавита или кириллицы, то это будет наш разделитель
+                    delimiter = text[i].ToString();
+                    //Предположим, что в задании подразумевается, что символ-разделитель всего один и выйдем из цикла
+                    break;
+                }
+            }
+            string[] words;
+            //Выделим перенос строки как разделитель тоже
+            if (delimiter != String.Empty)
+            {
+                char[] delimiters = new char[] { delimiter.ToCharArray()[0], '\r', '\n' };
+                words = text.Split(delimiters);
+            }
+            else
+            {
+                words = new string[1] {text};
+            }
+            string[] buffer = new string[words.Length];
+
+            for (int j = 0; j < words.Length; j++)
+            {
+                if (!buffer.Contains(words[j]) && (words[j] != String.Empty))
+                {
+                    result += words[j] + " occures " + Regex.Matches(text, words[j]).Count + " times \r\n";
+                    buffer[j] = words[j];
+                }
+            }
+            return (result != String.Empty) ? result : "Emptiness and Darkness...";
+        }
+    }
+}
